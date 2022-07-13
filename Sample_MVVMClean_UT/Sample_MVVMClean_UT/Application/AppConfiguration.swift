614d7242ffcf20b5eb5a048217a1de98fa3d146f
Gauri Sharma <gauri6404@gmail.com>
2022-07-13 17:53:36 +0530

    Developer Scheme configuration

--- Sample_MVVMClean_UT/Sample_MVVMClean_UT/Application/AppConfiguration.swift
+++ Sample_MVVMClean_UT/Sample_MVVMClean_UT/Application/AppConfiguration.swift
@@ -2,19 +2,19 @@ import Foundation
 
 final class AppConfiguration {
     lazy var apiKey: String = {
-        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
             fatalError("ApiKey must not be empty in plist")
         }
         return apiKey
     }()
     lazy var apiBaseURL: String = {
-        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "API_BASE_URL") as? String else {
             fatalError("ApiBaseURL must not be empty in plist")
         }
         return apiBaseURL
     }()
     lazy var imagesBaseURL: String = {
-        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "IMAGE_BASE_URL") as? String else {
             fatalError("ApiBaseURL must not be empty in plist")
         }
         return imageBaseURL
